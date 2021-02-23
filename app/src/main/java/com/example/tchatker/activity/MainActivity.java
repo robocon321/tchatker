4f040f2bd1bb029f7e7b8dee77c470bd9c521ab8
robocon321 <robocon321n@gmail.com>
2021-02-23 17:57:32 +0700

    Completed main activity

--- app/src/main/java/com/example/tchatker/activity/MainActivity.java
+++ app/src/main/java/com/example/tchatker/activity/MainActivity.java
@@ -1,16 +1,19 @@
-package com.example.tchatker;
+package com.example.tchatker.activity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.tchatker.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -38,5 +41,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                startActivity(intent);
+            }
+        }, 2000);
     }
 }
