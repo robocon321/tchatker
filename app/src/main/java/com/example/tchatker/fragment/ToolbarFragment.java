fdba4c450b980247928cabeb0b35715e8e89c037
robocon321 <robocon321n@gmail.com>
2021-02-26 19:55:03 +0700

    Create Toolbar

--- app/src/main/java/com/example/tchatker/fragment/ToolbarFragment.java
+++ app/src/main/java/com/example/tchatker/fragment/ToolbarFragment.java
@@ -15,7 +15,7 @@ public class ToolbarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.search_tool, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_tool, container, false);
         return view;
     }
 }
