56be3bbd73d547456d8808686ad173e50be30afc
robocon321 <robocon321n@gmail.com>
2021-02-27 23:29:55 +0700

    Implements Serializable to use in adapter

--- app/src/main/java/com/example/tchatker/model/Account.java
+++ app/src/main/java/com/example/tchatker/model/Account.java
@@ -3,7 +3,7 @@ package com.example.tchatker.model;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Account {
+public class Account implements Serializable{
     private String uname;
     private String pwd;
     private String name;
