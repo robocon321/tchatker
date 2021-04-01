55c4b82d5560c633d5b74e9689a1b1cf43892ebb
robocon321 <robocon321n@gmail.com>
2021-04-01 11:21:54 +0700

    Init Search tool and redesign Search Tool UI

--- app/src/main/java/com/example/tchatker/fragment/ToolbarFragment.java
+++ app/src/main/java/com/example/tchatker/fragment/ToolbarFragment.java
@@ -1,21 +1,59 @@
 package com.example.tchatker.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.tchatker.R;
+import com.example.tchatker.activity.SearchActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ToolbarFragment extends Fragment {
+    EditText editSearch;
+    ImageView imgBonus;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_tool, container, false);
+        init(view);
+        setEvents();
         return view;
+    }
+
+    public void init(View view){
+        editSearch = view.findViewById(R.id.editSearch);
+        imgBonus = view.findViewById(R.id.imgBonus);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+    }
+
+    public void setEvents() {
+        editSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
